0c501e2046c0ce4c2431c3183bb2f8929cb11802
IamKhant <104381819@students.swinburne.edu.my>
2026-02-22 16:42:52 +0800

    Enhance game round logging to include current scores for both players

--- script.js
+++ script.js
@@ -29,16 +29,16 @@ function playGame() {
   function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
       console.log("It's a tie!");
+      console.log("Both players chose " + humanChoice + ". Human current score: " + humanScore + ", Computer current score: " + computerScore);
     } else if (
       (humanChoice === "rock" && computerChoice === "scissor") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissor" && computerChoice === "paper")
     ) {
-
       humanScore++;
-      console.log("Human wins!" + humanChoice + " beats " + computerChoice + ". Human current score: " + humanScore);
+      console.log("Human wins!" + humanChoice + " beats " + computerChoice + ". Human current score: " + humanScore + ", Computer current score: " + computerScore);
     } else {
 
       computerScore++;
-      console.log("Computer wins! " + computerChoice + " beats " + humanChoice + ". Computer current score: " + computerScore);
+      console.log("Computer wins! " + computerChoice + " beats " + humanChoice + ". Computer current score: " + computerScore + ", Human current score: " + humanScore);
     }
   }
 
